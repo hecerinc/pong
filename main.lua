14ff8b32917cc27ae742208e931b686e91d444e6
Hector Rincon <hecerinc@hotmail.com>
2019-05-20 02:35:29 -0500

    Refactor objects into classes

--- main.lua
+++ main.lua
@@ -1,4 +1,8 @@
+-- main.lua
+--                    
+-- The main file for my Pong game!
 
+Class = require 'class'
 push = require 'push'
 
 WINDOW_WIDTH = 1280
@@ -9,6 +13,8 @@ VIRTUAL_HEIGHT = 243
 
 PADDLE_SPEED = 200
 
+require 'Paddle'
+require 'Ball'
 
 -- Set up function
 function love.load()
@@ -19,21 +25,19 @@ function love.load()
 
 	smallFont = love.graphics.newFont('font.ttf', 8)
 	love.graphics.setFont(smallFont)
+
 	push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
 		fullscreen = false,
 		resizable = false,
 		vsync = true
 	})
 
-	player1Y = 30
-	player2Y = VIRTUAL_HEIGHT - 50
+	player1 = Paddle(5, 30, 5, 20)
+	player2 = Paddle(VIRTUAL_WIDTH - 10, VIRTUAL_HEIGHT - 30, 5, 20)
 
 	-- Velocity & position variables for the ball
-	ballX = VIRTUAL_WIDTH / 2 - 2
-	ballY = VIRTUAL_HEIGHT / 2 - 2
+	ball = Ball(VIRTUAL_WIDTH / 2 - 2, VIRTUAL_HEIGHT / 2 - 2, 4, 4)
 
-	ballDX = math.random(2) == 1 and 100 or -100
-	ballDY = math.random(-50, 50)
 
 end
 
@@ -47,17 +51,24 @@ end
 -- Takes dt: delta time
 function love.update(dt)
 	if love.keyboard.isDown('w') then
-		player1Y = math.max(0, player1Y + -PADDLE_SPEED * dt)
+		player1.dy = -PADDLE_SPEED
 	elseif love.keyboard.isDown('s') then
-		player1Y = math.min(VIRTUAL_HEIGHT - 20, player1Y + PADDLE_SPEED * dt)
+		player1.dy = PADDLE_SPEED
+	else
+		player1.dy = 0
 	end
+
 	if love.keyboard.isDown('k') then
-		player2Y = math.max(0, player2Y + -PADDLE_SPEED * dt)
+		player2.dy = -PADDLE_SPEED
 	elseif love.keyboard.isDown('j') then
-		player2Y = math.min(VIRTUAL_HEIGHT - 20, player2Y + PADDLE_SPEED * dt)
+		player2.dy = PADDLE_SPEED
+	else
+		player2.dy = 0
 	end
-	ballX = ballX + ballDX * dt
-	ballY = ballY + ballDY * dt
+
+	ball:update(dt)
+	player1:update(dt)
+	player2:update(dt)
 end
 
 function love.draw()
@@ -66,12 +77,9 @@ function love.draw()
 	love.graphics.clear(40/255, 45/255, 52/255, 1)
 	love.graphics.printf('Hello Pong!', 0, 20, VIRTUAL_WIDTH, 'center')
 
-	-- Left paddle
-	love.graphics.rectangle('fill', 5, player1Y, 5, 20)
-	-- Right paddle
-	love.graphics.rectangle('fill', VIRTUAL_WIDTH - 10, player2Y, 5, 20)
-	-- Ball
-	love.graphics.rectangle('fill', ballX, ballY, 4, 4)
+	player1:render()
+	player2:render()
+	ball:render()
 
 	push:apply('end')
 end
